acf336c2d84230656eee892bcc48aae27ee1b15b
Stevelsa <13750555642@163.com>
2020-07-12 14:56:47 +0800

    delete some files

--- README.txt
+++
@@ -1,1 +0,0 @@
-this is repository for Python, created by Steve based on files in Thinkpad T490
--- rectangle.py
+++
@@ -1,8 +0,0 @@
-from turtle import *
-color('blue','blue')
-begin_fill()
-for i in range(6):
-    rt(60)
-    fd(90)
-done()
-end_fill()
